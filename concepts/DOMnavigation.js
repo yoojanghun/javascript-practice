be5756563efa096cce59e00409bc34f195d772ed
yoojanghun <yoojanghun@naver.com>
2025-06-27 11:57:15 +0900

    DOM navigation

--- concepts/DOMnavigation.js
+++ concepts/DOMnavigation.js
@@ -10,6 +10,7 @@
 // .parentElement
 // .children
 
+
 // --------- .firstElementChild --------------
 
 const element = document.getElementById("fruits");
@@ -20,3 +21,39 @@ const ulElements = document.querySelectorAll("ul");
 ulElements.forEach((ulElement) => {
     ulElement.firstElementChild.style.backgroundColor = "yellow";
 })
+
+
+// --------- .nextElementSibling --------------
+// --------- .previousElementSibling ----------
+
+const apple = document.getElementById("apple");
+const orange = apple.nextElementSibling;
+orange.style.backgroundColor = "orange";
+
+const fruits = document.getElementById("fruits");
+const vegetables = fruits.nextElementSibling;
+vegetables.style.backgroundColor = "green";
+
+const iceCream = document.getElementById("ice-cream");
+const pie = iceCream.previousElementSibling;
+pie.style.backgroundColor = "red";
+
+
+// --------- .parentElement -------------------
+
+const orange = document.getElementById("orange");
+const fruits = orange.parentElement;
+fruits.style.backgroundColor = "yellow";
+
+
+// -------------- .children -------------------
+
+const fruits = document.getElementById("fruits");
+const allChildren = fruits.children;
+console.log(allChildren);                   // HTML Collection
+
+Array.from(allChildren).forEach(child => {
+    child.style.backgroundColor = "yellow";
+})
+
+allChildren[1].style.color = "red";
